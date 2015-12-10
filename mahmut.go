846322c6885ab190731736d467bcdedd3d5c7c8f
Melih Mucuk <melihmucuk@gmail.com>
2015-12-10 12:06:53 +0200

    return err

--- mahmut.go
+++ mahmut.go
@@ -5,17 +5,18 @@ import (
 	"crypto/cipher"
 	"crypto/rand"
 	"encoding/base64"
+	"errors"
 	"fmt"
 	"io"
 )
 
 // Bagla encrypts the text with the given key.
-func Bagla(key string, text string) string {
+func Bagla(key string, text string) (string, error) {
 
 	k := len(key)
 	switch k {
 	default:
-		panic("key length must be 16, 24 or 32")
+		return "", errors.New("key length must be 16, 24 or 32")
 	case 16, 24, 32:
 		break
 	}
@@ -24,27 +25,27 @@ func Bagla(key string, text string) string {
 
 	block, err := aes.NewCipher([]byte(key))
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
 	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
 	iv := ciphertext[:aes.BlockSize]
 	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		panic(err)
+		return "", err
 	}
 
 	stream := cipher.NewCFBEncrypter(block, iv)
 	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
 
-	return base64.StdEncoding.EncodeToString(ciphertext)
+	return base64.StdEncoding.EncodeToString(ciphertext), nil
 }
 
 // Coz decrypts the cryptoText with the given key.
-func Coz(key string, cryptoText string) string {
+func Coz(key string, cryptoText string) (string, error) {
 	k := len(key)
 	switch k {
 	default:
-		panic("key length must be 16, 24 or 32")
+		return "", errors.New("key length must be 16, 24 or 32")
 	case 16, 24, 32:
 		break
 	}
@@ -53,11 +54,11 @@ func Coz(key string, cryptoText string) string {
 
 	block, err := aes.NewCipher([]byte(key))
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
 	if len(ciphertext) < aes.BlockSize {
-		panic("ciphertext too short")
+		return "", errors.New("ciphertext too short")
 	}
 	iv := ciphertext[:aes.BlockSize]
 	ciphertext = ciphertext[aes.BlockSize:]
@@ -65,5 +66,5 @@ func Coz(key string, cryptoText string) string {
 	stream := cipher.NewCFBDecrypter(block, iv)
 	stream.XORKeyStream(ciphertext, ciphertext)
 
-	return fmt.Sprintf("%s", ciphertext)
+	return fmt.Sprintf("%s", ciphertext), nil
 }
